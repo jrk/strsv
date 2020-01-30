a48f10e4b4d2a976aab1030807804f2d6c3eead2
Alex Reinking <alex.reinking@gmail.com>
2020-01-29 23:48:12 -0800

    Blocked solver is buggy but performs pretty much identically to BLAS.

--- src/blocked-solver.cpp
+++ src/blocked-solver.cpp
@@ -1,10 +1,48 @@
+#include <cstring>
 #include "solvers.h"
 
+typedef float v4sf __attribute__ ((vector_size (16), aligned(1)));
+
+// TODO: for now, assumes n%4=0
 void blocked_solver(LowerTriangularEquation &eq) {
     int n = eq.n;
-    for (int block = 0; block < n / 4; ++block) {
 
+    std::memset(eq.x.data(), 0, n * sizeof(float));
+
+    for (int block = 0; block < n / 4; ++block) {
+        int lowJ = 4 * block;
+        int lowI = 4 * block;
+
+        for (int j = 0; j < 4; ++j) {
+            int L_j = lowJ + j;
+            eq.x[L_j] += eq.b[L_j];
+            for (int i = j + 1; i < 4; ++i) {
+                int L_i = lowI + i;
+                eq.x[L_i] -= eq.x[L_j] * eq.L[L_i + n * L_j];
+            }
+        }
+
+        v4sf x_j0 = {eq.x[lowJ], eq.x[lowJ], eq.x[lowJ], eq.x[lowJ]};
+        v4sf x_j1 = {eq.x[lowJ + 1], eq.x[lowJ + 1], eq.x[lowJ + 1], eq.x[lowJ + 1]};
+        v4sf x_j2 = {eq.x[lowJ + 2], eq.x[lowJ + 2], eq.x[lowJ + 2], eq.x[lowJ + 2]};
+        v4sf x_j3 = {eq.x[lowJ + 3], eq.x[lowJ + 3], eq.x[lowJ + 3], eq.x[lowJ + 3]};
+        for (int square = block + 1; square < n / 4; ++square) {
+            int sqLowI = lowI + 4 * square;
+            v4sf x_i = *(v4sf *) (&eq.x[sqLowI]);
+
+            v4sf L_0 = *(v4sf *) (&eq.L[sqLowI + n * lowJ]);
+            v4sf L_1 = *(v4sf *) (&eq.L[sqLowI + n * (lowJ + 1)]);
+            v4sf L_2 = *(v4sf *) (&eq.L[sqLowI + n * (lowJ + 2)]);
+            v4sf L_3 = *(v4sf *) (&eq.L[sqLowI + n * (lowJ + 3)]);
+
+            x_i -= x_j0 * L_0 +
+                   x_j1 * L_1 +
+                   x_j2 * L_2 +
+                   x_j3 * L_3;
+
+            *(v4sf *) (&eq.x[sqLowI]) = x_i;
+        }
     }
 }
 
-//REGISTER_SOLVER(blocked_solver)
+REGISTER_SOLVER(blocked_solver)
--- tests/correctness_strsv.cpp
+++ tests/correctness_strsv.cpp
@@ -2,6 +2,8 @@
 #include "gtest/gtest.h"
 #include "solvers.h"
 
+#include <cstdio>
+
 bool relatively_close(const std::vector<float> &correct, const std::vector<float> &other) {
     float x_y = 0.0f;
     float y = 0.0f;
@@ -21,6 +23,7 @@ TEST(SolverTest, AllValuesClose) {
     auto correct_x = lte.x; // copies!
 
     for (const auto &s : Solvers::get_solvers()) {
+        printf("%s\n", s.first.c_str());
         for (int i = 0; i < 100; ++i) {
             // reset x
             std::fill(lte.x.begin(), lte.x.end(), 0.0f);
