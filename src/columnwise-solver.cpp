ec412beac54b1c65f140d9cda6f766d83bb3efff
Alex Reinking <alex.reinking@gmail.com>
2020-01-29 22:49:40 -0800

    Make columnwise solver which uses BLAS dot product to compute.

--- src/columnwise-solver.cpp
+++ src/columnwise-solver.cpp
@@ -3,12 +3,10 @@
 
 void columnwise_solver(LowerTriangularEquation &eq) {
     std::copy(eq.b.begin(), eq.b.end(), eq.x.begin());
-    cblas_strsv(CblasColMajor,
-                CblasLower,
-                CblasNoTrans,
-                CblasUnit,
-                eq.n, eq.L.data(), eq.n, eq.x.data(),
-                1);
+    int n = eq.n;
+    for (int i = 1; i < n; ++i) {
+        eq.x[i] -= cblas_sdot(i, &eq.L[i], n, &eq.x[0], 1);
+    }
 }
 
 REGISTER_SOLVER(columnwise_solver)
